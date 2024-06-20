a6cc49ee15c403b7ce7a9a110369ede55c123618
narf115 <145361225+narf115@users.noreply.github.com>
2024-06-20 09:30:57 +0200

    Update MovementBehaviour.cs
    Fix

--- Assets/Scripts/Player/MovementBehaviour.cs
+++ Assets/Scripts/Player/MovementBehaviour.cs
@@ -7,41 +7,31 @@ public class MovementBehaviour : MonoBehaviour
     
     [SerializeField]
     public float velocity=30;
-   
     Rigidbody2D _rb;
     public float jumpHeight;
-
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-      
     }
-    
-      public void Move(Vector3 direction)
-      {
+    public void Move(Vector3 direction)
+    {
           transform.position = transform.position + velocity * direction * Time.deltaTime;
-      }
-
-      public void Move(float vel, Vector3 dir)
-      {
+    }
+    public void Move(float vel, Vector3 dir)
+    {
           transform.position = transform.position + vel * dir * Time.deltaTime;
-      }
-      public void MovePosition(Vector3 direction)
-      {
+    }
+    public void MovePosition(Vector3 direction)
+    {
           GetComponent<Rigidbody2D>().MovePosition(transform.position + velocity * direction * Time.deltaTime);
-      }
-      public void MoveRB(Vector3 direction)
-      {
-          if(_rb!=null)
-          {
-
-
+    }
+    public void MoveRB(Vector3 direction)
+    {
+        if(_rb!=null)
+        {
               _rb.velocity = direction* velocity;
-
-
-          }
+        }
       }
-  
     public float GetVelocity()
       {
           return velocity;
@@ -49,17 +39,14 @@ public class MovementBehaviour : MonoBehaviour
     /*
     public void Move(Vector3 dir)
     {
-        // GetComponent<Rigidbody>().MovePosition(transform.position + speed * dir * Time.deltaTime);
-      // body.AddForce(-Physics.gravity, ForceMode.Acceleration);
+        GetComponent<Rigidbody>().MovePosition(transform.position + speed * dir * Time.deltaTime);
+        body.AddForce(-Physics.gravity, ForceMode.Acceleration);
         body.velocity = new Vector3(dir.x * speed, (dir.y+(-9.8f)*Time.deltaTime)*speed, dir.z*speed) ;
-        
-        
     }*/
     public void MoveToTarget(Vector3 _dir)
     {
         _rb.MovePosition(Vector3.MoveTowards(transform.position, _dir, velocity));
         _rb.velocity = _dir * velocity;
-        
     }
     public float SetSpeed(int s)
     {
